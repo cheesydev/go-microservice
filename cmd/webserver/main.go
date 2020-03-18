8d497dedcce9020d52785b23c273319bec4cea85
Rafael Portela <rafael@cheesy.dev>
2020-03-18 15:36:53 +0100

    Prometheus counter for pi http requests

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -7,16 +7,32 @@ import (
 
 	gomicroservice "github.com/cheesydev/go-microservice"
 	"github.com/cheesydev/go-microservice/pi"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	v := gomicroservice.Version()
-	fmt.Printf("Running go-microservice v%s, listening at port 8080\n", v)
 
 	http.HandleFunc("/", helloHandler)
-	http.HandleFunc("/pi", piHandler)
+
+	// example from https://github.com/prometheus/client_golang/blob/master/prometheus/promauto/auto.go
+	http.HandleFunc("/pi", promhttp.InstrumentHandlerCounter(
+		promauto.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "pi_requests_total",
+				Help: "Total number of pi http requests",
+			},
+			// partition the counter by http status code (a supported label)
+			[]string{"code"},
+		),
+		http.HandlerFunc(piHandler),
+	))
+
 	http.Handle("/metrics", promhttp.Handler())
+
+	fmt.Printf("Running go-microservice v%s, listening at port 8080\n", v)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
